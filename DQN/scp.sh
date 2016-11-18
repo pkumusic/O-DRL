c2913a33de9940f389be774c41f6d39080a71773
lyzmusic <635716260@qq.com>
2016-11-18 12:33:36 -0500

    - Clean files

--- DQN/scp.sh
+++ DQN/scp.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# DL Project for 10807
+# Author: Music, Tian, Jing, Yuxin
 mkdir $2
 scp music@shredder:~/tensorpack_music/examples/$1/train_log/$2/log* music@shredder:~/tensorpack_music/examples/$1/train_log/$2/latest music@shredder:~/tensorpack_music/examples/$1/train_log/$2/graph* music@shredder:~/tensorpack_music/examples/$1/train_log/$2/events* music@shredder:~/tensorpack_music/examples/$1/train_log/$2/checkpoint $2/
--- DQN/train.sh
+++ DQN/train.sh
@@ -1,2 +1,5 @@
-
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+# DL Project for 10807
+# Author: Music, Tian, Jing, Yuxin
 python DQN-gym-train.py --env MsPacman-v0
