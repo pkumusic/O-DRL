7592a3ff60e5fd81ecde9a59eb281023efa157bd
lyzmusic <635716260@qq.com>
2016-11-21 11:51:14 -0500

    - update scp

--- copy_models_from_server.sh
+++ copy_models_from_server.sh
@@ -1,2 +1,2 @@
-scp -r music@shredder:~/O-DRL/A3C/train_log A3C/train_log_copied
-scp -r music@shredder:~/O-DRL/DQN/train_log DQN/train_log_copied
+#!/usr/bin/env bash
+scp music@shredder:~/O-DRL/DQN/train_log/*/{log*,latest,graph*,events*,checkpoint} DQN/train_log_copied
