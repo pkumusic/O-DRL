d2308e0eaf4aebc4de0616149673ed3875ae52d1
lyzmusic <635716260@qq.com>
2016-11-25 13:47:57 -0500

    - update scp.sh

--- DQN/scp.sh
+++ DQN/scp.sh
@@ -4,4 +4,4 @@
 # Author: Music, Tian, Jing, Yuxin
 mkdir -p train_log_copied
 mkdir train_log_copied/$2
-scp music@shredder:~/O-DRL/DQN/train_log/$2/{log*,latest,graph*,events*,checkpoint} train_log_copied/$2
+scp music@shredder:~/O-DRL/DQN/train_log/$1/{log*,latest,graph*,events*,checkpoint} train_log_copied/$2
