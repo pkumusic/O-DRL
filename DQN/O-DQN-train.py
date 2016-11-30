d223864e7652fdd6e07590b96ede4b3ea82c84d4
lyzmusic <635716260@qq.com>
2016-11-30 18:11:27 -0500

    - init memory to 500

--- DQN/O-DQN-train.py
+++ DQN/O-DQN-train.py
@@ -55,7 +55,7 @@ END_EXPLORATION = 0.1
 MEMORY_SIZE = 1e6
 # NOTE: will consume at least 1e6 * 84 * 84 bytes == 6.6G memory.
 # Suggest using tcmalloc to manage memory space better.
-INIT_MEMORY_SIZE = 5e4
+INIT_MEMORY_SIZE = 5e2
 STEP_PER_EPOCH = 5000
 EVAL_EPISODE = 50
 
