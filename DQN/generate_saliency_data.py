0b6551d69f05906981308c12a1bcc12b568db446
Tian Tian <tian.tianclaire@gmail.com>
2017-09-21 14:27:26 -0700

    remove colorbar

--- DQN/generate_saliency_data.py
+++ DQN/generate_saliency_data.py
@@ -24,7 +24,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
 import pickle
-from matplotlib_colorbar.colorbar import Colorbar
+#from matplotlib_colorbar.colorbar import Colorbar
 
 IMAGE_SIZE = (84, 84)
 FRAME_HISTORY = 4
